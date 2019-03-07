5a07de4da3e5737bceaeea444b191ee776752f24
Albert Kaaman <albert@kaaman.nu>
2019-03-07 23:14:26 +0100

    refactor: trying to improve virtual table performance

--- packages/vuetify/src/components/VDataTable/VVirtualTable.ts
+++ packages/vuetify/src/components/VDataTable/VVirtualTable.ts
@@ -1,8 +1,10 @@
 import './VVirtualTable.sass'
 
-import { VNode } from 'vue'
+import { VNode, VNodeChildren } from 'vue'
 import { convertToUnit } from '../../util/helpers'
 import VSimpleTable from './VSimpleTable'
+
+let cachedItems: VNode[] = []
 
 export default VSimpleTable.extend({
   name: 'v-virtual-table',
@@ -24,7 +26,10 @@ export default VSimpleTable.extend({
   },
 
   data: () => ({
-    scrollTop: 0
+    scrollTop: 0,
+    cachedItems: null as VNodeChildren,
+    oldStart: 0,
+    scrollDebounce: null as any
   }),
 
   computed: {
@@ -52,11 +57,28 @@ export default VSimpleTable.extend({
   },
 
   watch: {
-    scrollTop: 'setScrollTop'
+    scrollTop: 'setScrollTop',
+    startIndex (newValue, oldValue) {
+      this.oldStart = oldValue
+    }
   },
 
   mounted () {
+    const table = this.$refs.table as Element
+    table.addEventListener('mousewheel', this.onMousewheel, { passive: true })
+
     this.setScrollTop()
+
+    for (let i = this.startIndex; i < this.stopIndex; i++) {
+      cachedItems.push(this.$scopedSlots.item!({ index: i }) as any)
+    }
+
+    this.$forceUpdate()
+  },
+
+  beforeDestroy () {
+    const table = this.$refs.table as Element
+    table.removeEventListener('mousewheel', this.onMousewheel)
   },
 
   methods: {
@@ -66,16 +88,16 @@ export default VSimpleTable.extend({
       }
     },
     setScrollTop () {
-      const wrapper = this.$refs.wrapper as Element
-      wrapper.scrollTop = this.scrollTop
+      const table = this.$refs.table as Element
+      table.scrollTop = this.scrollTop
     },
     genScroller () {
       return this.$createElement('div', {
         ref: 'scroller',
         staticClass: 'v-virtual-table__scroller',
-        style: {
-          top: `${this.scrollTop}px`
-        },
+        // style: {
+        //   top: `${this.scrollTop}px`
+        // },
         on: {
           scroll: (e: Event) => {
             const target = e.target as Element
@@ -89,31 +111,68 @@ export default VSimpleTable.extend({
       ])
     },
     genBody () {
+      const diff = Math.abs(this.oldStart - this.startIndex)
+      if (diff > 100) {
+        cachedItems = []
+        for (let i = this.startIndex; i < this.stopIndex; i++) {
+          cachedItems.push(this.$scopedSlots.item!({ index: i }) as any)
+        }
+      } else if (this.oldStart < this.startIndex) {
+        const diff = this.startIndex - this.oldStart
+
+        for (let i = diff; i > 0; i--) {
+          cachedItems.shift()
+          cachedItems.push(this.$scopedSlots.item!({ index: this.stopIndex - i }) as any)
+        }
+      } else if (this.startIndex < this.oldStart) {
+        const diff = this.oldStart - this.startIndex
+
+        // console.log('diff', diff)
+
+        for (let i = diff - 1; i >= 0; i--) {
+          cachedItems.pop()
+          cachedItems.unshift(this.$scopedSlots.item!({ index: this.startIndex + i }) as any)
+        }
+      }
+
+      this.oldStart = this.startIndex
+
       return this.$createElement('tbody', [
         this.$createElement('tr', { style: this.createStyleHeight(this.offsetTop) }),
-        this.$scopedSlots.items ? this.$scopedSlots.items({ start: this.startIndex, stop: this.stopIndex }) : null,
+        cachedItems,
         this.$createElement('tr', { style: this.createStyleHeight(this.offsetBottom) })
       ])
     },
-    genWrapper () {
+    onMousewheel (e: Event) {
+      const newScrollTop = Math.max(0, Math.min(this.totalHeight, this.scrollTop + (e as WheelEvent).deltaY))
+      this.scrollTop = newScrollTop
+    },
+    genTable () {
       return this.$createElement('div', {
-        ref: 'wrapper',
-        staticClass: 'v-virtual-table__wrapper',
-        style: {
-          height: convertToUnit(this.height)
-        },
-        on: {
-          mousewheel: (e: WheelEvent) => {
-            const scroller = this.$refs.scroller as Element
-            scroller.scrollTop = Math.max(0, Math.min(this.totalHeight, this.scrollTop + e.deltaY))
-          }
-        }
+        ref: 'table',
+        staticClass: 'v-virtual-table__table'
+        // on: {
+        //   mousewheel: (e: WheelEvent) => {
+        //     const scroller = this.$refs.scroller as Element
+        //     scroller.scrollTop = Math.max(0, Math.min(this.totalHeight, this.scrollTop + e.deltaY))
+        //   }
+        // }
       }, [
         this.$createElement('table', [
           this.$slots['body.before'],
           this.genBody(),
           this.$slots['body.after']
-        ]),
+        ])
+      ])
+    },
+    genWrapper () {
+      return this.$createElement('div', {
+        staticClass: 'v-virtual-table__wrapper',
+        style: {
+          height: convertToUnit(this.height)
+        }
+      }, [
+        this.genTable(),
         this.genScroller()
       ])
     }
